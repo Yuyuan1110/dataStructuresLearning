2162afa11a283c5457b07799bc84759d6b2f8867
Yuyuan <z0975750027@gmail.com>
2023-12-25 11:16:22 +0800

    complete array search.

--- Advanced/Arrays/arraysADT.cpp
+++ Advanced/Arrays/arraysADT.cpp
@@ -22,34 +22,101 @@ void Append(struct Array *arr, int x)
 {
     if (arr->length < arr->size)
     {
-        arr->A[arr->length++] = x; 
+        arr->A[arr->length++] = x;
     }
 }
 
-void Insert(struct Array *arr, int index,int x){
+void Insert(struct Array *arr, int index, int x)
+{
     int i;
-    if(index > 0 && index <= arr->length);
-        for(i=arr->length; i>index; i--){
-            arr->A[i] = arr->A[i-1];
-        }
-        arr->A[index]=x;
-        arr->length++;
+    if (index > 0 && index <= arr->length)
+        ;
+    for (i = arr->length; i > index; i--)
+    {
+        arr->A[i] = arr->A[i - 1];
+    }
+    arr->A[index] = x;
+    arr->length++;
 }
 
-void Delete(struct Array *arr, int x){
+void Delete(struct Array *arr, int x)
+{
     int i;
-    if(x >= 0 && x <= arr->length){
-        for(i = x; i < arr->length; i++){
-            arr->A[i] = arr->A[i+1];
+    if (x >= 0 && x <= arr->length)
+    {
+        for (i = x; i < arr->length; i++)
+        {
+            arr->A[i] = arr->A[i + 1];
         }
         arr->length--;
     }
 }
+
+int RBinarySearch(int a[], int l, int h, int key)
+{
+    int mid;
+    if (l < h)
+    {
+        mid = (l + h) / 2;
+        if (key == a[mid])
+        {
+            return mid;
+        }
+        else if (key < a[mid])
+        {
+            return RBinarySearch(a, l, mid - 1, key);
+        }
+        else
+        {
+            return RBinarySearch(a, mid + 1, h, key);
+        }
+    }
+}
+int binarySearch(struct Array arr, int key)
+{
+    int l, mid, h;
+    l = 0;
+    h = arr.length - 1;
+
+    while (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (key == arr.A[mid])
+        {
+            return mid;
+        }
+        else if (key < arr.A[mid])
+        {
+
+            h = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return 0;
+}
+int linearSearch(struct Array arr, int x)
+{
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    struct Array arr = {{2, 3, 4}, 10, 3};
-    Append(&arr, 10);
-    Delete(&arr, 0);
+    struct Array arr = {{2, 3, 4, 5, 7}, 10, 5};
+    // Append(&arr, 10);
+    // Delete(&arr, 0);
     Display(arr);
+    // int a = linearSearch(arr, 3);
+    int a = binarySearch(arr, 7);
+    cout << a << endl;
     return 0;
 }
