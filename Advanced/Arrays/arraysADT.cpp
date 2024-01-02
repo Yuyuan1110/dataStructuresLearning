4a88028d89168c2870855c8ec4740eca1c1a741f
Yuyuan <z0975750027@gmail.com>
2024-01-02 17:45:17 +0800

    add array merge function.

--- Advanced/Arrays/arraysADT.cpp
+++ Advanced/Arrays/arraysADT.cpp
@@ -127,13 +127,52 @@ int LinearSearch(struct Array arr, int x)
     return -1;
 }
 
+struct Array* Merge(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+    struct Array *arr3 = new struct Array;
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+        {
+            arr3->A[k++] = arr1->A[i++];
+        }
+        else if (arr1->A[i] > arr2->A[j])
+        {
+            arr3->A[k++] = arr2->A[j++];
+        }
+        else if (arr1->A[i] == arr2->A[j])
+        {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+
+    for (; i < arr1->length; i++)
+    {
+        arr3->A[k++] = arr1->A[i];
+    }
+    for (; j < arr2->length; j++)
+    {
+        arr3->A[k++] = arr2->A[j];
+    }
+
+    arr3->length = k;
+    arr3->size = 10;
+    return arr3;
+}
+
 int main()
 {
-    struct Array arr = {{2, 3, 4, 5, 7}, 10, 5};
+    // struct Array arr = {{2, 3, 4, 5, 7}, 10, 5};
     // Append(&arr, 10);
     // Delete(&arr, 0);
-    Reserve(&arr);
-    Display(arr);
+    // Reserve(&arr);
+    struct Array arr1 = {{1,3,5,7,9},10,5};
+    struct Array arr2 = {{0,2,4,6,8},10,5};
+    struct Array *arr3 = Merge(&arr1, &arr2);
+    Display(*arr3);
     // int a = linearSearch(arr, 3);
     // int a = binarySearch(arr, 7);
     // cout << a << endl;
