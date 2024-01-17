3fffed638ca7171edca2689b74829263b5fbb3dc
Yuyuan <z0975750027@gmail.com>
2024-01-17 21:55:46 +0800

    try to create Spare class, to be continued

--- Advanced/Matrix/matrixSparse.cpp
+++ Advanced/Matrix/matrixSparse.cpp
@@ -1,4 +1,4 @@
-#iinclude < iostream>
+#include <iostream>
 using namespace std;
 
 class Element
@@ -9,19 +9,34 @@ private:
     int x;
 
 public:
-    Element()
+    Element() : i(0), j(0), x(0)
     {
-        i = 0;
-        j = 0;
-        x = 0;
     }
-    Element(int i, int j, int x)
+    Element(int i, int j, int x) : i(i), j(j), x(x)
     {
+    }
+    int getI(){
+        return i;
+    }
+
+    int getJ(){
+        return j;
+    }
+
+    int getX(){
+        return x;
+    }
+
+    void setI(int i){
         this->i = i;
+    }
+    void setJ(int j){
         this->j = j;
+    }
+    void setX(int x){
         this->x = x;
     }
-    ~Element();
+
 };
 
 class Sparse
@@ -33,28 +48,98 @@ private:
     Element *e;
 
 public:
-    Sparse(){
-        this->n=0;
-        this->m=0;
-        this->num=0;
-        this->e->~Element();
-    };
-    Sparse(int n, int m, int num, Element e){
-        this->n=n;
-        this->m=m;
-        this->num=num;
-        this->e = e;
+    Sparse(int n, int m, int num, Element *e) : n(n), m(m), num(num)
+    {
+        this->e = new Element[num];
+        for (int i = 0; i < num; i++)
+        {
+            this->e[i] = e[i];
+        }
     }
-    ~Sparse();
+    int getM() const
+    {
+        return m;
+    }
+
+    int getN() const
+    {
+        return n;
+    }
+
+    int getNUM() const
+    {
+        return num;
+    }
+
+    Element *getE() const
+    {
+        return e;
+    }
+
+    void setM(int value)
+    {
+        m = value;
+    }
+
+    void setN(int value)
+    {
+        n = value;
+    }
+
+    void setNUM(int value)
+    {
+        num = value;
+    }
+
+    void setE(Element *elements, int count)
+    {
+        delete[] e; // 釋放原有的內存
+        e = new Element[num];
+        for (int i = 0; i < count; i++)
+        {
+            e[i] = elements[i];
+        }
+    }
 };
 
-void Create(class Sparse *s){
+void Create(class Sparse *s)
+{
+    int n, m, num;
+    cout << "Enter Dimensions: " << endl;
+    cin >> m >> n;
+    s->setM(m);
+    s->setN(n);
+    cout << "Number of non-zero: " << endl;
+    cin >> num;
+    s->setNUM(num);
 
-    
+    Element e[num];
+    for (int k = 0; k < num; k++)
+    {
+        int i, j, x;
+        cin >> i, j, x;
+        e[k] = Element(i,j,x);
+    }
+    s->setE(e, num);
+}
+
+void Display(Sparse s){
+    int i,j,k=0;
+    for(i=0; i < s.getM(); i++){
+        for(j=0; j < s.getN(); j++){
+            if(i == s.getE()[k].getI() && j == s.getE()[k].getJ()){
+                cout << s.getE()[k++].getX() << " ";
+            }else{
+                cout << "0 " ;
+            }
+        }
+        cout << endl;
+    }
 }
 
 int main()
 {
+    Sparse s = new Sparse();
 
     return 0;
 }
