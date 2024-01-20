d913e5e29e33b2bcd58540701a84ac621969c063
Yuyuan <z0975750027@gmail.com>
2024-01-20 15:35:03 +0800

    override iostream and +, completed Sparse matrix

--- Advanced/Matrix/matrixSparse.cpp
+++ Advanced/Matrix/matrixSparse.cpp
@@ -18,6 +18,9 @@ private:
     Element *ele;
 
 public:
+    Sparse() : m(0), n(0), num(0), ele(NULL){
+
+    }
     Sparse(int m, int n, int num)
     {
         this->m = m;
@@ -30,40 +33,127 @@ public:
         delete[] ele;
     }
 
-    void read()
-    {
-        cout << "Enter non-zero elements" << endl;
-        for (int i = 0; i < num; i++)
-        {
-            cin >> ele[i].i >> ele[i].j >> ele[i].x;
-        }
-    }
-    void display()
-    {
-        int k = 0;
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (ele[k].i == i && ele[k].j == j)
-                {
-                    cout << ele[k++].x << " ";
-                }
-                else
-                {
-                    cout << "0 ";
-                }
-                
-            }
-            cout << endl;
-        }
-    }
+    Sparse operator+(Sparse &s);
+    friend istream &operator>>(istream &is, Sparse &s);
+    friend ostream &operator<<(ostream &os, Sparse &s);
 };
+
+Sparse Sparse::operator+(Sparse &s)
+{
+    int i = 0, j = 0, k = 0;
+    if (m != s.m || n != s.n)
+    {
+        return Sparse();
+    }
+    Sparse *sum = new Sparse(m, n, num + s.num);
+    while (i < num && j < s.num)
+    {
+        if (ele[i].i < s.ele[j].i)
+        {
+            sum->ele[k++] = ele[i++];
+        }
+        else if (ele[i].i > s.ele[j].i)
+        {
+            sum->ele[k++] = s.ele[j++];
+        }
+        else
+        {
+            if (ele[i].j < s.ele[j].j)
+            {
+                sum->ele[k++] = ele[i++];
+            }
+            else if (ele[i].j > s.ele[j].j)
+            {
+                sum->ele[k++] = s.ele[j++];
+            }
+            else
+            {
+                sum->ele[k] = ele[i];
+                sum->ele[k++].x = ele[i++].x + s.ele[j++].x;
+            }
+        }
+    }
+    for (; i < num; i++)
+    {
+        sum->ele[k++] = ele[i];
+    }
+    for (; j < s.num; j++)
+    {
+        sum->ele[k++] = s.ele[j];
+    }
+    sum->num = k;
+    return *sum;
+}
+
+istream &operator>>(istream &is, Sparse &s)
+{
+    cout << "Enter non-zero elements" << endl;
+    for (int i = 0; i < s.num; i++)
+    {
+        cin >> s.ele[i].i >> s.ele[i].j >> s.ele[i].x;
+    }
+    return is;
+}
+ostream &operator<<(ostream &os, Sparse &s)
+{
+    int k = 0;
+    for (int i = 0; i < s.m; i++)
+    {
+        for (int j = 0; j < s.n; j++)
+        {
+            if (s.ele[k].i == i && s.ele[k].j == j)
+            {
+                cout << s.ele[k++].x << " ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+    return os;
+}
+
+//     void read()
+//     {
+//         cout << "Enter non-zero elements" << endl;
+//         for (int i = 0; i < num; i++)
+//         {
+//             cin >> ele[i].i >> ele[i].j >> ele[i].x;
+//         }
+//     }
+//     void display()
+//     {
+//         int k = 0;
+//         for (int i = 0; i < m; i++)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 if (ele[k].i == i && ele[k].j == j)
+//                 {
+//                     cout << ele[k++].x << " ";
+//                 }
+//                 else
+//                 {
+//                     cout << "0 ";
+//                 }
+
+//             }
+//             cout << endl;
+//         }
+//     }
+// };
 
 int main()
 {
-    Sparse s1(5,5,5);
-    s1.read();
-    s1.display();
+    Sparse s1(5, 5, 5);
+    cin >> s1;
+    cout << s1;
+    Sparse s2(5, 5, 5);
+    cin >> s2;
+    cout << s2;
+    Sparse sum = s1 + s2;
+    cout << endl << "sum matrux is: " << endl << sum;
     return 0;
 }
